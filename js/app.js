defe2676979a70bf5eed07b35560ff7e1c7f3e94
Tawfiek <tawfiek.108@gmail.com>
2020-06-20 12:00:12 +0200

    feat: link darksky API and setup view

--- js/app.js
+++ js/app.js
@@ -1,23 +1,35 @@
+let UNIT = 'us';
+let TIME_RANGE= 'hourly';
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    _addingSegmentClickListener();
+    _addingToggleClickListener();
     _detailsSectionScrollableByMouse();
+    _addingSegmentClickListener();
 });
 
+_getForecastData().then(_render);
 
-
-function _addingSegmentClickListener () {
+/**
+ * adding toggle functionality and handling on temp toggle
+ * @private
+ */
+function _addingToggleClickListener () {
     const buttons = document.querySelectorAll('#units-section div.toggle button');
     buttons.forEach((btn, index) => {
         btn.onclick = () => {
             const otherBtnIndex = index ? 0 : 1;
             btn.classList.add("selected");
             buttons[otherBtnIndex].classList.remove("selected");
+            UNIT = btn.innerText === 'C' ? 'si' : 'us';
+            _getForecastData(UNIT).then(_render);
         }
-    })
-    console.log('#DEBUG => ', buttons);
+    });
 }
 
+/**
+ * make details Section Scrollable with mouse whale
+ * @private
+ */
 function _detailsSectionScrollableByMouse() {
     const target = document.querySelector('.details-section')
 
@@ -26,11 +38,114 @@ function _detailsSectionScrollableByMouse() {
         const toRight = event.deltaY > 0 && target.scrollLeft < target.scrollWidth - target.clientWidth
 
         if (toLeft || toRight) {
-            // event.preventDefault()
+            event.preventDefault()
             target.scrollLeft += event.deltaY
         }
     });
 }
 
+/**
+ * adding toggle functionality and handling on details section
+ * @private
+ */
+function _addingSegmentClickListener () {
+    const tabs = document.querySelectorAll('.forecast div.segment .tab');
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            const otherBtnIndex = index ? 0 : 1;
+            tab.classList.add("selected");
+            tabs[otherBtnIndex].classList.remove("selected");
+            TIME_RANGE = tab.innerText.toLowerCase();
+            _getForecastData().then(_renderDetailsSection);
+        });
+    });
+}
+
+async function _getForecastData (unit = UNIT) {
+    console.log('#DEBUG unit => ', unit)
+    return  await Service.getForecastData(unit);
+}
 
 
+function _render (forecastData) {
+    _renderMainSection(forecastData);
+    _renderDetailsSection(forecastData);
+}
+
+/**
+ * Render the details section of forecast
+ * @param forecastData: forecast data array that comes from API
+ * @param timeRange {'hourly' | 'daily' }: the segment value.
+ * @private
+ */
+function _renderDetailsSection(forecastData, timeRange = TIME_RANGE) {
+    _addDetailsSection ();
+    const detailsSection = document.querySelector('.forecast .details-section');
+    console.log(timeRange);
+    const dataArray = forecastData[timeRange]['data'];
+    let timeFormat = timeRange === "hourly"? 'HH:mm': 'ddd D';
+
+    dataArray.forEach((item, i) => {
+        let itemElement;
+        if (timeRange === 'hourly') itemElement = i ?  _makeItem(item, timeFormat) : _makeItem(item, '[Now]');
+        else  itemElement = i ?  _makeItem(item, timeFormat) : _makeItem(item, '[Today]');
+        detailsSection.appendChild(itemElement);
+    });
+
+    function _makeItem (itemData, timeFormat) {
+        const item = document.createElement('div');
+        item.className = 'forecast-item';
+
+        const timeItem = document.createElement('div');
+        timeItem.className = 'time';
+        timeItem.innerText = moment(itemData['time'] * 1000).format(timeFormat);
+
+        const iconItem = document.createElement('img');
+        iconItem.src = itemData['icon'].startsWith('clear')? 'assets/images/clear.svg':'assets/images/cloudy.svg';
+
+        const degreeItem = document.createElement('div');
+        degreeItem.className = 'today-degree';
+        if (!itemData['temperature']) {
+            degreeItem.innerHTML = `${itemData['temperatureMax'].toFixed(0)}&#730/${itemData['temperatureMin'].toFixed(0)}&#730`;
+        }
+        else degreeItem.innerHTML = `${itemData['temperature'].toFixed(0)}&#730`;
+
+        item.append(timeItem, iconItem, degreeItem);
+
+        return item;
+    }
+
+    function _addDetailsSection() {
+        const section = document.querySelector('.forecast .details-section');
+        const newSection = document.createElement('div');
+        newSection.className = 'details-section';
+        if (section) section.remove();
+        document.querySelector('.forecast').append(newSection);
+        _detailsSectionScrollableByMouse();
+    }
+}
+
+function _renderMainSection (forecastData) {
+    const currentData = forecastData['currently']
+    const todayDate = moment(currentData['time'] * 1000).format('dddd DD, YYYY');
+    document.getElementById('city-name').innerText = 'New Cairo';
+    document.getElementById('today-date').innerText = todayDate;
+
+    const currentStatusElem = document.getElementById('current-description')
+    currentStatusElem.innerText = currentData['summary'];
+
+    const leftCol =  document.querySelector('.main-data .left-col')
+    const currentIcon = document.createElement('img');
+    currentIcon.src = currentData['icon'].startsWith('clear')? 'assets/images/clear.svg':'assets/images/cloudy.svg';
+    currentIcon.alt = 'Current weather';
+
+    const imgs = leftCol.getElementsByTagName('img');
+    if (imgs.length) leftCol.removeChild(imgs[0]);
+    leftCol.insertBefore( currentIcon , currentStatusElem);
+
+    const todayData = forecastData['daily']['data'][0];
+    document.getElementById('today-degree').innerHTML = currentData['temperature'].toFixed(0)+'&#730';
+    document.getElementById('from-to').innerHTML
+        = `${todayData['temperatureMax'].toFixed(0)}&#730 / ${todayData['temperatureMin'].toFixed(0)}&#730`;
+    document.getElementById('long-description').innerText = todayData['summary'];
+}
--- js/service.js
+++ js/service.js
@@ -1,1 +1,65 @@
-const request = require('request');
+
+class Service {
+
+    /**
+     * Makes a fetch request to get forecast data based on location.
+     * @param unit {'us' | 'si'}: unit stander
+     * @return {Promise<ReadableStream<Uint8Array>>}
+     */
+    static getForecastData (unit= "us") {
+        return new Promise((async (resolve, reject) => {
+            try {
+                const cord = await this._getLocation();
+                console.log('#DEBUG cord ', cord);
+
+                // using external proxy because darksky blocks origins for security reasons
+                // for more info check https://darksky.net/dev/docs/faq#cross-origin
+                const CORS_API_HOST = 'cors-anywhere.herokuapp.com';
+                const CORS_API_URL = 'https://' + CORS_API_HOST + '/';
+
+                const URL = CORS_API_URL + this._getDarkSkyAPIURL(cord.lat, cord.long, unit);
+                console.log('#DEBUG url => ', URL);
+                const xhr = new XMLHttpRequest();
+
+                xhr.open('GET', URL, true);
+                xhr.send();
+                xhr.onload = () => {
+                    if (xhr.status === 200) {
+                        const data = JSON.parse(xhr.responseText);
+                        return resolve(data);
+                    }
+                }
+                xhr.onerror = reject;
+            }catch (e) {
+                reject(e);
+            }
+        }));
+    }
+    /**
+     * build the final dark sky API URL using location information.
+     * @param lat {Number}: latitude coordination
+     * @param long {Number}: longitude coordination
+     * @param unit {'us' | 'si'}: unit stander
+     * @return {string}: final URL
+     * @private
+     */
+    static _getDarkSkyAPIURL (lat, long, unit= "us") {
+        const API_KEY = 'a177f8481c31fa96c3f95ad4f4f84610';
+
+        return `https://api.darksky.net/forecast/${API_KEY}/${lat},${long}?exclude=alerts,flags,minutely&units=${unit}`;
+    }
+
+    /**
+     * this function gets the location of the user
+     * @return {Promise<{long: Number, lat: Number}>}:
+     * that resolves with coordination log and lat if the user gives the permission
+     */
+    static _getLocation () {
+        return new Promise(((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition((pos) => {
+               const cord = {long: pos.coords.longitude, lat: pos.coords.latitude};
+               resolve(cord)
+            }, reject, {enableHighAccuracy: true});
+        }))
+    }
+}
